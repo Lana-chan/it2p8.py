bd8b68766bbad069e41f0fe7b75117022b776d4d
Erin Pinheiro <Lana-chan@users.noreply.github.com>
2019-07-11 05:17:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# it2p8.py
+This is a simple Python script to generate Pico-8 music files from `.it` files. The objective in doing this is to make it easier to compose for Pico-8 by freeing composers from the constrained Pico-8 music editor while still obeying the sound engine limitations.
+
+### Disclaimer
+
+This tool was developed lazily for my own personal usage. I have not made efforts in making it particularly user-friendly or even crash-proof. You are welcome to improve on it for yourself or submit issues with suggestions and requests, but as it stands, it's a tool aimed at myself alone, and the code is very jank in places. Nevertheless, I wanted to share it with others since I believe in always sharing my processes and methods.
+
+## Usage
+
+The converter is called by:
+
+```python3 it2p8.py input.it [output.p8]```
+
+where `input.it` is a specifically-designed ImpulseTracker module. I recommend OpenMPT, but any other ImpulseTracker editor should work fine.
+
+### How the `.it` file must be structured
+
+The converter looks at the orderlist and pattern data alone. Right now, everything else is ignored. Your file must have 4 channels and your patterns must be all 32 rows in length.
+
+The included `test.it` file in this repository contains samples of the standard instruments 0-7, plus some custom SFX instruments. In order to use other SFX instruments 8-F, you must sample them yourself and include them in order after the standard instruments. The converter looks at what instrument number is being used in order to determine the Pico-8 instrument, so they must be in the same order. You must also include the SFX strings of your custom instruments in the song message in order for them to be included back in the output pico-8 cartridge. The root note used is C-5 in IT, matching to C-2 in Pico-8.
+
+The volume column is parsed and remapped from a range of 0-64 to 0-7 to fit the pico-8 volume. This gives you less direct control over the final output, for the sake of more accurate playback when hearing your song as the original `.it` file.
+
+The effects column is mapped in a similar way in order to match the sound between playbacks, however with obvious limitations. Effect command values are ignored, since they aren't present in pico-8. The mapping is as follows:
+
+| IT effect | Pico-8 effect       |
+| --------- | -------------       |
+| `Dx0`     | Effect 4 - Fade In  |
+| `D0x`     | Effect 5 - Fade Out |
+| `Gxx`     | Effect 1 - Slide    |
+| `Hxx`     | Effect 2 - Vibrato  |
+| `Exx`     | Effect 3 - Drop     |
+
+## FAQ
+
+1. Why didn't you program in the arpeggio effects 6 and 7?
+
+Personally, I don't like those effects since I have never been able to get a good effect out of them, and thus prefer using SFX instruments for arp macros. Additionally, the way the `Jxy` command works for arpeggio in the ImpulseTracker format can't be easily translated over to the Pico-8 arp commands, which use 4 notes.
+
+## TODO: Improvements I want to make some day
+
+1. A second script which generates an `.it` samplepack based on a Pico-8 cartridge with the custom SFX instruments so you don't have to build one by hand.
+
+2. Make it so note-cuts and note-offs are parsed in a way that allows the `.it` input to be composed in the standard way rather than needing every note to be inputted in the same way the Pico-8 editor needs them to be.
+
+3. Do not include Pico-8 effect 3 in SFX instruments (retrigger) if instruments have loops (such as arpeggio macros)
+
+## License
+
+I am releasing this code under the Mozilla license since I want others to be able to build upon this if they want to compose for Pico-8 games more easily. If you do use this in your project (either as a tool or bundling it in somewhere else) please credit me somewhere, and please let me know! I'd love to hear from others using what I've made.
---
+++ it2p8.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python3
+
+### This Source Code Form is subject to the terms of the Mozilla Public
+### License, v. 2.0. If a copy of the MPL was not distributed with this
+### file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# by maple syrup <maple@maple.pet>
+
+import sys
+import os
+import struct
+import pprint
+
+# loads impulsetracker module information
+class ImpulseTracker:
+	# takes filename, passes it to parser
+	def read_file(self, filename):
+		# check if file exists?
+		if(not os.path.exists(filename)):
+			print("File not found: ", filename)
+			return -1
+		
+		# parse file
+		with open(filename, 'rb') as f:
+			data = f.read()
+			return self.parse_it(data)
+
+	# takes binary content, fills self values
+	def parse_it(self, data):
+		# reference: https://github.com/schismtracker/schismtracker/wiki/ITTECH.TXT
+
+		# check header
+		header = struct.unpack_from("4s", data)
+		if(header[0] != b'IMPM'):
+			print("Not an Impulse Tracker module")
+			return -1
+
+		(OrdNum, InsNum, SmpNum, PatNum, Special) = struct.unpack_from("4H6xH", data, offset=0x20)
+
+		# if there's a song message:
+		if(Special & 0b1):
+			# for some reason this wasn't working as a single unpack line with format "HI"
+			(MsgLength,) = struct.unpack_from("H", data, offset=0x36)
+			(MsgOffset,) = struct.unpack_from("I", data, offset=0x38)
+			(message,) = struct.unpack_from("{}s".format(MsgLength), data, offset=MsgOffset)
+			message = message.replace(b'\r',b'\n')
+			self.message = message.decode("ascii")
+
+		# read orderlist
+		Orderlist = struct.unpack_from("{}B".format(OrdNum), data, offset=0xC0)
+		# filter to valid pattern numbers 0<=x<=199
+		self.orderlist = list(filter(lambda x: x >= 0 and x <= 199, Orderlist))
+
+		# patterns start at offset = int at 00C0h+OrdNum+InsNum*4+SmpNum*4
+		PatOffset = struct.unpack_from("{}I".format(PatNum), data, offset=0xC0+OrdNum+InsNum*4+SmpNum*4)
+
+		# read each pattern
+		for o in PatOffset:
+			(Length, Rows) = struct.unpack_from("2H", data, offset=o)
+			(PatData,) = struct.unpack_from("{}s".format(Length), data, offset=o+8)
+			Pattern = self.parse_pattern(PatData, Rows)
+			self.pattern.append(Pattern)
+
+		return 0
+
+	def parse_pattern(self, data, rows):
+		Pattern = []
+		offset = 0
+
+		prevmskvar = {}
+		prevnote = {}
+		previnst = {}
+		prevvol = {}
+		prevcmd = {}
+	
+		row = {}
+
+		while offset < len(data):
+			# chvar = channel variable
+			chnvar = data[offset]
+			offset += 1
+			if(chnvar == 0):
+				# end of row
+				Pattern.append(row)
+				row = {}
+				continue
+			Chn = (chnvar-1) & 63
+
+			note = None
+			inst = None
+			volp = None
+			cmd = [None, None]
+			
+			# mskvar = mask variable
+			if(chnvar & 128):
+				mskvar = data[offset]
+				prevmskvar[Chn] = mskvar
+				offset += 1
+			else:
+				mskvar = prevmskvar[Chn]
+
+			if(mskvar & 1):
+				# read note
+				note = data[offset]
+				if(note > 119):
+					note = None
+				offset += 1
+				prevnote[Chn] = note
+			
+			if(mskvar & 2):
+				# read instrument
+				inst = data[offset]
+				offset += 1
+				previnst[Chn] = inst
+			
+			if(mskvar & 4):
+				# read vol/pan
+				volp = data[offset]
+				offset += 1
+				prevvol[Chn] = volp
+			
+			if(mskvar & 8):
+				# read command & value
+				cmd = [data[offset], data[offset+1]]
+				offset += 2
+				prevcmd[Chn] = cmd
+
+			if(mskvar & 16):
+				note = prevnote[Chn]
+			if(mskvar & 32):
+				inst = previnst[Chn]
+			if(mskvar & 64):
+				volp = prevvol[Chn]
+			if(mskvar & 128):
+				cmd = prevcmd[Chn]
+
+			if(note or inst or volp or cmd != [None, None]):
+				row[Chn] = (note, inst, volp, cmd)
+
+		return Pattern
+
+	def __init__(self, filename=None):
+		pass
+		# initialize
+		self.orderlist = []
+		self.pattern = []
+		self.comment = ""
+
+		if(filename != None):
+			self.read_file(filename)
+
+def interpol(value, leftMin, leftMax, rightMin, rightMax):
+	# Figure out how 'wide' each range is
+	leftSpan = leftMax - leftMin
+	rightSpan = rightMax - rightMin
+
+	# Convert the left range into a 0-1 range (float)
+	valueScaled = float(value - leftMin) / float(leftSpan)
+
+	# Convert the 0-1 range into a value in the right range.
+	return rightMin + (valueScaled * rightSpan)
+
+def it_to_p8(it):
+	output = """\
+pico-8 cartridge http://www.pico-8.com
+version 16
+__lua__
+
+__sfx__
+"""
+
+	# objective:
+	# convert each channel into sfx string
+	# reorganize orderlist into p8 __music__
+
+	sfxs = []
+	music = []
+	effectmap = {7: 1, 8: 2, 5: 3}
+
+	# load custom sfx instruments
+	if(it.message):
+		sfxs= it.message.split("\n")
+
+	for p in it.orderlist:
+		# p is a pattern index in the .it orderlist
+
+		pat = it.pattern[p]
+		curpat = []
+
+		for ch in range(0,4):
+			speed = 10
+			cursfx = "01{:02x}0000".format(speed)
+			prevnote = 0
+			previnst = 0
+
+			# turn channel into sfx
+			for row in pat:
+				try:
+					data = row[ch]
+				except KeyError:
+					# empty row
+					cursfx += "00000"
+					prevnote = 0
+					previnst = 0
+					continue
+
+				try:
+					note = data[0]-36
+				except TypeError:
+					# note = NoneType
+					note = prevnote
+
+				inst = data[1]-1
+				if(inst == None):
+					inst = previnst
+
+				vol = data[2]
+				if(vol == None):
+					vol = 64
+				vol = int(interpol(vol,0,64,0,7))
+
+				effect = 0
+				iteffect = data[3]
+				if(iteffect[0] in effectmap):
+					effect = effectmap[iteffect[0]]
+				if(iteffect[0] == 4):
+					if(iteffect[1] & 0x0F):
+						# fade out
+						effect = 5
+					if(iteffect[1] & 0xF0):
+						# fade in
+						effect = 4
+
+				# pico8 needs sfx retrigger
+				if(inst > 7 and previnst == inst and effect == 0):
+					effect = 3
+
+				prevnote = note
+				previnst = inst
+
+				rowstr = "{:02x}{:1x}{:1x}{:1x}".format(note, inst, vol, effect)
+				cursfx += rowstr
+			
+			# if sfx is blank, just add turned off channel to music and continue loop
+			if(cursfx[8:] == "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"):
+				curpat.append(64)
+			else:
+				# check if sfx exists
+				try:
+					index = sfxs.index(cursfx)
+					# if yes, add index of match to music
+					curpat.append(index)
+				except:
+					# if not, increase sfx counter, add sfx to list, add index to music
+					curpat.append(len(sfxs))
+					sfxs.append(cursfx)
+
+		music.append("{:02x}{:02x}{:02x}{:02x}".format(curpat[0],curpat[1],curpat[2],curpat[3]))
+
+	# make it so there are loop markers around first and last patterns
+	# this isn't pretty.
+	output += "\n".join(sfxs)
+	output += "\n\n__music__"
+	output += "\n01 {}\n00 ".format(music[0])
+	output += "\n00 ".join(music[1:-1])
+	output += "\n02 {}".format(music[-1])
+
+	return output
+
+if __name__ == "__main__":
+	# check if sys.argv length is right
+	if(len(sys.argv) < 2):
+		print("usage: {} input.it [output.p8]".format(sys.argv[0]))
+		sys.exit()
+
+	try:
+		output = sys.argv[2]
+	except IndexError:
+		output = "output.p8"
+
+	# take sys.argv[1] into read_file and do stuff
+	it = ImpulseTracker(sys.argv[1])
+	out = it_to_p8(it)
+
+	with open(output, "w+") as f:
+		f.write(out)
